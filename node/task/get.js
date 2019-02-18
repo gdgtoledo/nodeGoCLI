ac77f317fb637651e361029a7257e658a35b89d8
Javier López de Ancos <javierlopezdeancos@outlook.com>
2019-02-18 18:47:50 +0100

    remove check if description is a string

--- node/task/get.js
+++ node/task/get.js
@@ -1,15 +1,5 @@
-const { error } = require('../logs/error');
-
 const getIndex = (description = '', tasksToDo = []) => {
-  const FLAG_ERROR = 'Error: task is not a string';
-  const taskDescriptionIsAString = typeof description === 'string';
   const hasTaskToDo = tasksToDo.length > 0;
-
-  if (!taskDescriptionIsAString) {
-    error(FLAG_ERROR);
-    process.stdout.write('\n');
-    process.exit(1);
-  }
 
   if (!hasTaskToDo) {
     return -1;
