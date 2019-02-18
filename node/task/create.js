a5bfae89f202a94695fb6ab3b5e3b9c1c5f9c543
Javier López de Ancos <javierlopezdeancos@outlook.com>
2019-02-18 18:46:08 +0100

    remove check for is string the description option

--- node/task/create.js
+++ node/task/create.js
@@ -1,20 +1,11 @@
 const tasks = require('../tasks');
 const { Task } = require('./Task');
 const { success } = require('../logs/success');
-const { error } = require('../logs/error');
 
 const create = async (description, isComplete) => {
-  const NO_CORRECT_DESCRIPTION_ERROR = 'Error: task is not a string';
   const CREATE_SUCCESS = `"${description}" has being added to your todo list`;
-  const taskDescriptionIsAString = typeof description === 'string';
 
   process.stdout.write('\n');
-
-  if (!taskDescriptionIsAString) {
-    error(NO_CORRECT_DESCRIPTION_ERROR);
-    process.stdout.write('\n');
-    process.exit(1);
-  }
 
   const tasksToDo = await tasks.get();
   const newTaskToDo = new Task(description, isComplete);
